5cbc70fbaadf7aa5de50ff2708e8202876779cd9
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-02-08 14:27:39 +0100

    Created project

---
+++ Mood Food.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mood_Food.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
