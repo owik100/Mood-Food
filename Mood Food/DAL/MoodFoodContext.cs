d1929e7f57f7695abf3a23398b3149e7862cc65d
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-02-13 21:57:04 +0100

    Added Data Access Layer - Context for Entity Framework

---
+++ Mood Food/DAL/MoodFoodContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+using Mood_Food.Models;
+
+namespace Mood_Food.DAL
+{
+    public class MoodFoodContext :DbContext
+    {
+        MoodFoodContext() : base("MoodFoodDatabase")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
