9df801175a0983fdecb4ce81f6a24d526053f817
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-02-16 13:32:37 +0100

    Added Unit test to database

---
+++ Mood Food.Tests/DatabaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Mood_Food.DAL;
+
+namespace Mood_Food.Tests
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void CanConnectToDatabase()
+        {
+            var context = new MoodFoodContext();
+            
+                bool connected;
+                try
+                {
+                    context.Database.Connection.Open();
+                    context.Database.Connection.Close();
+                }
+                catch (SqlException)
+                {
+                    connected = false;
+                }
+                connected = true;
+
+            Assert.AreEqual(true, connected);          
+        }
+    }
+}
