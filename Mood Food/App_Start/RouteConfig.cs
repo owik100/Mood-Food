92f7b7c6b01fc46d3656e94e98ab860e0cec2de0
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-02-18 22:58:49 +0100

    Added route for static sites

--- Mood Food/App_Start/RouteConfig.cs
+++ Mood Food/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace Mood_Food
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+             name: "Static",
+             url: "Info/{name}",
+             defaults: new { controller = "Info", action = "StaticSite" }
+         );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Mood Food/Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mood_Food.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult StaticSite(string name)
+        {
+            return View(name);
+        }
+    }
+}
