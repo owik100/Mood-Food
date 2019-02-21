6de6d7056b2a11ef7d2c95e152976480cfe8a08e
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-02-21 12:38:43 +0100

    Added extension method to get images paths

---
+++ Mood Food/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mood_Food.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string ImagePath(this UrlHelper helper, string imageName)
+        {
+            string path = System.Configuration.ConfigurationManager.AppSettings["ImagePath"];
+            return Path.Combine(path + imageName);
+        }
+
+        public static string ProductImagePath(this UrlHelper helper, string imageName)
+        {
+            string path = System.Configuration.ConfigurationManager.AppSettings["ProductsImagePath"];
+            return Path.Combine(path + imageName);
+        }
+
+        public static string CategoriesImagePath(this UrlHelper helper, string imageName)
+        {
+            string path = System.Configuration.ConfigurationManager.AppSettings["CategoriesImagePath"];
+            return Path.Combine(path + imageName);
+        }
+    }
+}
