2908694ecc91b9bd9e8fc4d7eddf0f774672fc70
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2019-03-02 15:41:41 +0100

    Working on ShoppingCart. Added session manager

---
+++ Mood Food/Infrastructure/ShoppingCartManager.cs
@@ -0,0 +1,23 @@
+﻿using Mood_Food.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mood_Food.Infrastructure
+{
+    public class ShoppingCartManager
+    {
+        ISessionManager session;
+        MoodFoodContext db;
+
+        ShoppingCartManager(ISessionManager session, MoodFoodContext db)
+        {
+            this.session = session;
+            this.db = db;
+        }
+
+
+
+    }
+}
